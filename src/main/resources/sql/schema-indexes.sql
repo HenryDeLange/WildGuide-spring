80507e947c8353da0c28dfe526a4b128e61655cb
Henry <36891536+HenryDeLange@users.noreply.github.com>
2025-01-10 13:38:26 +0200

    fix column name

--- src/main/resources/sql/schema-indexes.sql
+++ src/main/resources/sql/schema-indexes.sql
@@ -3,7 +3,3 @@ CREATE INDEX IF NOT EXISTS idx_users_username ON "users"(username);
 
 -- GUIDES
 CREATE INDEX IF NOT EXISTS idx_guides_visibility ON "guides"(visibility);
-
--- GUIDE ENTRIES
-CREATE INDEX IF NOT EXISTS idx_guides_name ON "guide_entries"(name);
-CREATE INDEX IF NOT EXISTS idx_guides_scientific_name ON "guide_entries"(scientificName);
