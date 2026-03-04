4bade9afbf07b2a4aefd9b443886f649fab019f2
Henry <36891536+HenryDeLange@users.noreply.github.com>
2026-03-04 08:41:48 +0200

    upgrade to spring boot 4, improved error handling

--- src/main/java/mywild/wildguide/framework/error/ErrorConfig.java
+++ src/main/java/mywild/wildguide/framework/error/ErrorConfig.java
@@ -1,5 +1,7 @@
 package mywild.wildguide.framework.error;
 
+import org.jspecify.annotations.NonNull;
+import org.jspecify.annotations.Nullable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
@@ -12,8 +14,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.lang.NonNull;
-import org.springframework.lang.Nullable;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -38,8 +38,10 @@ public class ErrorConfig {
 
         @ExceptionHandler(Exception.class)
         public final ResponseEntity<Object> handleAllExceptions(Exception ex, WebRequest request) {
+            String url = null;
             if (request instanceof ServletWebRequest servletWebRequest) {
                 log.debug("Caused By URL: {}", servletWebRequest.getRequest().getRequestURL());
+                url = servletWebRequest.getRequest().getRequestURL().toString();
             }
             try {
                 return super.handleException(ex, request);
@@ -48,21 +50,38 @@ public class ErrorConfig {
                 if (notHandledException instanceof AbstractAppException appException) {
                     String translatedMessage = messageSource.getMessage(
                         appException.getMessage(), null, appException.getMessage(), request.getLocale());
-                    log.error(translatedMessage, appException);
+                    if (appException.getMessage().equals("file.not-found")) {
+                        log.debug(translatedMessage);
+                    }
+                    else {
+                        log.error(translatedMessage, appException);
+                    }
                     ResponseStatus responseStatus = AnnotationUtils.findAnnotation(ex.getClass(), ResponseStatus.class);
                     HttpStatus status = (responseStatus != null) ? responseStatus.code() : HttpStatus.INTERNAL_SERVER_ERROR;
-                    return handleExceptionInternal(appException, translatedMessage,
-                        new HttpHeaders(), status, request);
+                    return handleExceptionInternal(
+                        appException,
+                        getJsonBody(translatedMessage, appException.getLocalizedMessage(), url),
+                        new HttpHeaders(),
+                        status,
+                        request);
                 }
                 else if (notHandledException instanceof DbActionExecutionException dbException) {
                     log.error(dbException.getMessage(), dbException);
-                    return handleExceptionInternal(dbException, dbException.getMessage(),
-                        new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+                    String translatedMessage = messageSource.getMessage(
+                        "db.error", null, dbException.getLocalizedMessage(), request.getLocale());
+                    return handleExceptionInternal(dbException,
+                        getJsonBody(translatedMessage, dbException.getLocalizedMessage(), url),
+                        new HttpHeaders(),
+                        HttpStatus.BAD_REQUEST,
+                        request);
                 }
                 else  {
                     log.error(notHandledException.getMessage(), notHandledException);
-                    return handleExceptionInternal(notHandledException, notHandledException.getMessage(),
-                        new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR, request);
+                    return handleExceptionInternal(notHandledException,
+                        notHandledException.getMessage(),
+                        new HttpHeaders(),
+                        HttpStatus.INTERNAL_SERVER_ERROR,
+                        request);
                 }
             }
         }
@@ -85,8 +104,28 @@ public class ErrorConfig {
         @NonNull
         protected ResponseEntity<Object> createResponseEntity(@Nullable Object body, @NonNull HttpHeaders headers,
                 @NonNull HttpStatusCode statusCode, @NonNull WebRequest request) {
-            String jsonBody = "{ \"reason\": \"" + body + "\" }";
-            return new ResponseEntity<>(jsonBody, headers, statusCode);
+            return new ResponseEntity<>(body, headers, statusCode);
+        }
+
+        private String getJsonBody(String reason, String details, String url) {
+            return String.format("{ \"reason\": %s, \"details\": %s, \"url\": %s }", 
+                escapeJsonString(reason), 
+                escapeJsonString(details),
+                escapeJsonString(url));
+        }
+
+        private String escapeJsonString(String value) {
+            if (value == null) {
+                return "\"\"";
+            }
+            return "\""
+                + value
+                    .replace("\\", "\\\\")
+                    .replace("\"", "\\\"")
+                    .replace("\n", "\\n")
+                    .replace("\r", "\\r")
+                    .replace("\t", "\\t")
+                + "\"";
         }
 
     }
